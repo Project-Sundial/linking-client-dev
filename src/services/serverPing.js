ad80a9c61fc59114ea27e754d60ca783a919aa51
davidwperez@gmail.com <davidwperez@gmail.com>
2023-10-23 22:52:43 -0400

    Add monitor api post request
    Co-authored-by: Jacob Clark <jacobclark1198@gmail.com>
    Co-authored-by: marymcdonald <10123446+marymcdonald@users.noreply.github.com>
    Co-authored-by: sofalere <sofalere@gmail.com>

--- src/services/serverPing.js
+++ src/services/serverPing.js
@@ -16,6 +16,17 @@ export const pingMonitor = async (ping, endpointKey) => {
   }
 };
 
+export const addMonitor = async ({ schedule, command }) => {
+  try {
+    console.log(BASE_URL + CREATE_MONITOR);
+    console.log(`Initial job: ${schedule} ${command}`);
+    const { data } = await axios.post(BASE_URL + PING_MONITOR + endpointKey, { schedule, command });
+    return data;
+  } catch (e) {
+    console.log(e);
+  }
+}
+
 // export const createMonitor = async (newMonitor) => {
 //   const { data } = await axios.post(BASE_URL + CREATE_MONITOR, newMonitor);
 //   return data;
