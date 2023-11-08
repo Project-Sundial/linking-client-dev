b1978219fc11fc738c9d4c22b54f97536f04166f
davidwperez@gmail.com <davidwperez@gmail.com>
2023-11-08 00:21:11 -0500

    Placeholder register route
    Co-authored-by: Jacob Clark <jacobclark1198@gmail.com>
    Co-authored-by: marymcdonald <10123446+marymcdonald@users.noreply.github.com>
    Co-authored-by: sofalere <sofalere@gmail.com>

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,4 +1,5 @@
 // export const BASE_URL = 'http://localhost:58669'; // changes with droplet
 export const PING_MONITOR = '/api/pings/';
 export const CREATE_MONITOR = '/api/monitors';
+export const CREATE_MACHINE = '/api/machines';
 export const GET_MONITORS = '/api/monitors';
