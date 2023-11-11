93ef84d85c3a78cf401f93d0a6a8ad3a5198e021
davidwperez@gmail.com <davidwperez@gmail.com>
2023-11-11 00:25:49 -0500

    Listen command
    Co-authored-by: Jacob Clark <jacobclark1198@gmail.com>
    Co-authored-by: marymcdonald <10123446+marymcdonald@users.noreply.github.com>
    Co-authored-by: sofalere <sofalere@gmail.com>

---
+++ src/commands/listen.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import { exec } from 'child_process';
+import { EXECUTABLE_PATH } from '../constants/paths.js';
+import { LISTENER_PORT } from "../constants/ports.js";
+
+export const listen = () => {
+  const app = express();
+  let counter = 0;
+
+  app.post('/trigger-sync', (req, res) => {
+    counter += 1;
+    exec(executablePath, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error: ${error.message}`);
+        return;
+      }
+      if (stderr) {
+        console.error(`stderr: ${stderr}`);
+        return;
+      }
+      console.log(`Output: ${stdout}`);
+    });
+    console.log(`Received trigger request. Initiating sundial update request number ${counter}.`);
+    res.status(200).send('CLI update call initiated. Count: ');
+  });
+  
+  app.listen(LISTENER_PORT, () => {
+    console.log(`Remote host daemon listening on ${LISTENER_PORT}`);
+  });
+}
