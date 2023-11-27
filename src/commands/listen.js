6bfed346401ab477a970f6e05b75a2632a11e57d
davidwperez@gmail.com <davidwperez@gmail.com>
2023-11-27 14:41:16 -0800

    Co-authored-by: Jacob Clark <jacobclark1198@gmail.com> Co-authored-by: marymcdonald <10123446+marymcdonald@users.noreply.github.com> Co-authored-by: sofalere <sofalere@gmail.com>

--- src/commands/listen.js
+++ src/commands/listen.js
@@ -42,7 +42,7 @@ export const listen = () => {
 
   app.post('/trigger-sync', (req, res) => {
     const executablePath = EXECUTABLE_PATH;
-    // console.log(process);
+    console.log(process.pid);
     // console.log(process.execPath);
     // console.log(process.argv[1]);
     const args = [process.argv[1], 'update'];
