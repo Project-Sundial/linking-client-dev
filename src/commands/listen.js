bc1f483b1f0f423db7e517f40b4014b5b0e4a6e4
davidwperez@gmail.com <davidwperez@gmail.com>
2023-11-27 16:57:13 -0800

    Log listen server
    Co-authored-by: Jacob Clark <jacobclark1198@gmail.com>
    Co-authored-by: marymcdonald <10123446+marymcdonald@users.noreply.github.com>
    Co-authored-by: sofalere <sofalere@gmail.com>

--- src/commands/listen.js
+++ src/commands/listen.js
@@ -1,12 +1,64 @@
+// import express from 'express';
+// import { spawn } from 'child_process';
+// import { EXECUTABLE_PATH } from '../constants/paths.js';
+// import { LISTENER_PORT } from '../constants/ports.js';
+
+// export const listen = () => {
+//   const app = express();
+
+//   app.post('/trigger-sync', (req, res) => {
+//     const executablePath = EXECUTABLE_PATH;
+//     const args = [process.argv[1], 'update'];
+
+//     const process1 = spawn(executablePath, args);
+
+//     process1.stdout.on('data', (data) => {
+//       console.log(`Output: ${data}`);
+//     });
+
+//     process1.stderr.on('data', (data) => {
+//       console.error(`stderr: ${data}`);
+//     });
+
+//     process1.on('close', (code) => {
+//       if (code !== 0) {
+//         console.error(`Error: Process exited with code ${code}`);
+//       }
+//     });
+
+//     console.log('Received trigger request. Initiating sundial update request.');
+//     res.status(200).send('CLI update call initiated.');
+//   });
+
+//   app.listen(LISTENER_PORT, () => {
+//     console.log(`Host daemon listening on ${LISTENER_PORT}`);
+//   });
+// };
+
+
 import express from 'express';
 import { spawn } from 'child_process';
 import { EXECUTABLE_PATH } from '../constants/paths.js';
 import { LISTENER_PORT } from '../constants/ports.js';
+import fs from 'fs';
 
 export const listen = () => {
   const app = express();
+  let count = 0;
 
   app.post('/trigger-sync', (req, res) => {
+    count += 1;
+
+    // Log count to file
+    const logFilePath = '/etc/sundial-listen.log';
+    const logMessage = `Trigger count: ${count}\n`;
+
+    fs.appendFile(logFilePath, logMessage, (err) => {
+      if (err) {
+        console.error('Error writing to log file:', err);
+      }
+    });
+
     const executablePath = EXECUTABLE_PATH;
     const args = [process.argv[1], 'update'];
 
@@ -14,6 +66,11 @@ export const listen = () => {
 
     process1.stdout.on('data', (data) => {
       console.log(`Output: ${data}`);
+      fs.appendFile(data, logMessage, (err) => {
+        if (err) {
+          console.error('Error 2: writing to log file:', err);
+        }
+      });
     });
 
     process1.stderr.on('data', (data) => {
