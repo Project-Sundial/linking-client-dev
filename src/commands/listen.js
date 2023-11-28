a8d895a0fefdd80834db00b7a4cfe59ca5f1cd3e
davidwperez@gmail.com <davidwperez@gmail.com>
2023-11-27 17:06:17 -0800

    more logging
    Co-authored-by: Jacob Clark <jacobclark1198@gmail.com>
    Co-authored-by: marymcdonald <10123446+marymcdonald@users.noreply.github.com>
    Co-authored-by: sofalere <sofalere@gmail.com>

--- src/commands/listen.js
+++ src/commands/listen.js
@@ -64,9 +64,33 @@ export const listen = () => {
 
     const process1 = spawn(executablePath, args);
 
-    process1.stdout.on('data', (data) => {
-      console.log(`Output: ${data}`);
-      fs.appendFile(data, logMessage, (err) => {
+    process1.stdout.on('data', async (data) => {
+      const fetchCrontab = async () => {
+        const retrieveCurrentCrontab = async () => {
+          return new Promise((resolve, reject) => {
+            exec('crontab -l', (error, stdout, stderr) => {
+              if (error || stderr) {
+                console.error(`Error listing crontab: ${error} ${stderr}`);
+                reject(error);
+                return;
+              }
+        
+              resolve(stdout);
+            });
+          });
+        };
+      
+        try {
+          const currentCrontab = await retrieveCurrentCrontab();
+          console.log('Current crontab:', currentCrontab );
+          return currentCrontab;
+        } catch (error) {
+          console.error('An error occurred:', error);
+        }
+      }
+      let logMessage = await fetchCrontab()
+
+      fs.appendFile(logFilePath, logMessage, (err) => {
         if (err) {
           console.error('Error 2: writing to log file:', err);
         }
